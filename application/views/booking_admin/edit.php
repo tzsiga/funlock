1f0f7461fda903cc32b597d055c3b9acbdaec238
tzsiga <tzsiga@gmail.com>
2013-07-29 12:47:54 +0200

    some booking/edit tidy up

--- application/views/booking_admin/edit.php
+++ application/views/booking_admin/edit.php
@@ -8,35 +8,35 @@
     <?= $this->session->flashdata('msg') ?>
   </h3>
   <?php
-    echo form_open('admin/booking/edit/'.$booking->id);
-    echo '<p>';
+    echo form_open('admin/booking/edit/'.$booking->id, array('id' => 'booking-edit-form'));
+    echo '<p class="row">';
     echo form_label('Aktív?', 'status');
     echo form_checkbox(array('name' => 'status', 'id' => 'status', 'value' => 'active', 'checked' => ($booking->status == 'active') ? true : false));
-    echo '</p><p>';
+    echo '</p><p class="row">';
     echo form_label('Fizetve?', 'payment_verified');
     echo form_checkbox(array('name' => 'payment_verified', 'id' => 'payment_verified', 'value' => 'yes', 'checked' => ($booking->payment_verified == 'yes') ? true : false));
-    echo '</p><p>';
     if (isset($voucher)) {
+      echo '</p><p class="row">';
       echo '<label for="">Kupon: </label>';
-      echo $voucher->code.' ('.$voucher->discounted_price.' Ft)';
-      echo '</p><p>';
+      echo '<a href="'.base_url().'index.php/admin/voucher/edit/'.$voucher->id.'">'.$voucher->code.'</a> ('.$voucher->discounted_price.' Ft)';
     }
+    echo '</p><p>';
     echo form_label('Foglaló vezetékneve', 'book-fname');
     echo form_input(array('name' => 'book-fname', 'id' => 'book-fname', 'value' => $booking->book_fname));
     echo '</p><p>';
     echo form_label('Foglaló keresztneve', 'book-sname');
     echo form_input(array('name' => 'book-sname', 'id' => 'book-sname', 'value' => $booking->book_sname));
-    echo '</p><p>';
+    echo '</p><p class="row">';
     echo form_label('Foglalt időpont', 'appointment');
     echo form_input(array('name' => 'appointment', 'id' => 'appointment', 'value' => date('Y-m-d', $booking->appointment))).' - ';
     echo form_dropdown('appointment-hour', Utils::getPlaytimeRangeDropdownValues(), date('G', $booking->appointment) + (date('i', $booking->appointment) == 30 ? 0.5 : 0));
-    echo '</p><p>';
+    echo '</p><p class="row">';
     echo form_label('Fizetés átutalással', 'payment-option');
     echo form_radio(array('name' => 'payment-option', 'id' => 'payment-option-1', 'value' => 'card', 'checked' => ($booking->payment_option == 'card' ? true : false)));
     if ($booking->payment_option == 'card') {
         echo ' <strong>Kód: '.$code.'</strong>';
     }
-    echo '</p><p>';
+    echo '</p><p class="row">';
     echo form_label('Fizetés készpénzzel', 'payment-option');
     echo form_radio(array('name' => 'payment-option', 'id' => 'payment-option-2', 'value' => 'cache', 'checked' => ($booking->payment_option == 'cache' ? true : false)));
     echo '</p><p>';
@@ -66,13 +66,13 @@
     echo '</p><p>';
     echo form_label('Adószám', 'tax-number');
     echo form_input(array('name' => 'tax-number', 'id' => 'tax-number', 'value' => ($booking->tax_number == 0 ? '' : $booking->tax_number)));
-    echo '</p><p>';
+    echo '</p><p class="row">';
     echo form_label('Megjegyzések', 'comment');
     echo form_textarea(array('name' => 'comment', 'id' => 'comment', 'value' => $booking->comment, 'rows' => 6, 'cols' => 54));
-    echo '</p><p>';
+    echo '</p><p class="row">';
     echo form_label('Jegyzetek', 'notes');
     echo form_textarea(array('name' => 'notes', 'id' => 'notes', 'value' => $booking->notes, 'rows' => 6, 'cols' => 54));
-    echo '</p><p>';
+    echo '</p><p class="row">';
     echo form_label('Foglalás időpontja', 'booking-date');
     echo form_input(array('name' => 'booking-date', 'id' => 'booking-date', 'value' => date("Y-m-d H:i", $booking->booking_date)));
     echo '</p><p>';
