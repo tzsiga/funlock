63cba588579781dd6674a5f8d80f1bc5c4f5f072
tzsiga <tzsiga@gmail.com>
2014-06-15 20:56:18 +0200

    CodeIgniter 2.2 upgrade

--- create_phar.php
+++ create_phar.php
@@ -4,14 +4,14 @@ $filename = "funlock";
 $lang = "hu";
 
 $phar = new Phar(
-		"phar/".$filename."_".$lang.".phar",
-		0,
-		$filename.".phar"
+  "phar/".$filename."_".$lang.".phar",
+  0,
+  $filename.".phar"
 );
 
 $phar->buildFromDirectory(
-    dirname(__FILE__), 
-    "/(\.php)|(\.png)|(\.jpg)|(\.gif)/"   // |(\.html)|(\.htaccess)
+  dirname(__FILE__), 
+  "/(\.php)|(\.png)|(\.jpg)|(\.gif)/"   // |(\.html)|(\.htaccess)
 );
 
 $phar->setStub($phar->createDefaultStub("index.php"));
