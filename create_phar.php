7078ffbff39d59d3d2dcda14363a093e2b16ef84
tzsiga <tzsiga@gmail.com>
2014-06-21 11:00:14 +0200

    smarter phar creation

--- create_phar.php
+++ create_phar.php
@@ -1,17 +1,17 @@
 <?php
 
 $filename = "funlock";
-$lang = "hu";
 
-$phar = new Phar(
-  "phar/".$filename."_".$lang.".phar",
-  0,
-  $filename.".phar"
-);
+if (count($argv) == 2) {
+  $suffix = $argv[1];
+} else {
+  $suffix = time();
+}
+
+$phar = new Phar("phar/".$filename."_".$suffix.".phar", 0, $filename.".phar");
 
 $phar->buildFromDirectory(
-  dirname(__FILE__), 
-  "/(\.php)|(\.png)|(\.jpg)|(\.gif)/"   // |(\.html)|(\.htaccess)
+  dirname(__FILE__), "/(\.php)|(\.png)|(\.jpg)|(\.gif)/"   // |(\.html)|(\.htaccess)
 );
 
 $phar->setStub($phar->createDefaultStub("index.php"));
